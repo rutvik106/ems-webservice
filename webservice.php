13915b5be4d92af65cda0f7e803c501e7891520e
rutvik <rutvik106@live.com>
2016-08-23 17:14:53 +0530

    new changes

--- webservice.php
+++ webservice.php
@@ -16,6 +16,7 @@ require_once ('../lib/enquiry-group-functions.php');
 require_once ('../lib/lead-functions.php');
 require_once("../lib/rel-enquiry-group-functions.php");
 require_once("../lib/quantity-functions.php");
+require_once("../lib/prefix-functions.php");
 
 
 switch($_POST["method"]){
@@ -26,38 +27,220 @@ switch($_POST["method"]){
 
 	$enquiry=getEnquiryById($enquiry_form_id);
 
+	$enquiryDetails=getEnquiryById($enquiry_form_id);
+
 	$customer_id =$enquiry["customer_id"]; //$enquiryDetails['customer_id'];
+
+
+	$customer_detail_array=getCustomerById($customer_id);
+
+	$prefix=getPrefixById($customer_detail_array["prefix_id"]);	
+
+	$customer_detail_array["prefix"]=$prefix["prefix"];
+
+	$tNumber = getNoOfEnquiriesForCustomerId($customer_id);
+	$sNumber = getNoOfSuccessfullEnquiriesForCustomerId($customer_id);     
+
+	$enquiry["total_enquiry"]=$sNumber.'/'.$tNumber; 
+
+
+	$group="";
+
+	$groupNameDetailsArray = getEnquiryGroupNamesByEnquiryId($enquiry_form_id);
+	foreach($groupNameDetailsArray as $groupNameArray)
+	{
+		$group= $group . $groupNameArray['enquiry_group_name']. ", ";  
+	}  
+
+	$enquiry["group"]=$group;
+
+	//$prefix_only=array("prefix"=>$prefix["prefix"]);
+
+	//array_merge($customer_detail_array,$prefix_only);
 
 	//echo json_encode($enquiry);
 
-	$root=array( "enquiry"=>$enquiry,
-							"customer"=>getCustomerById($customer_id),
-							"contact"=>getCustomerContactNo($customer_id),
-							"follow_up_details"=>getFollowUpDetailsByEnquiryId($enquiry_form_id));
-	
+
+
+
+	$subCategory = getSubCatFromEnquiryId($enquiry_form_id);
+	if(is_numeric($subCategory[0][0]))
+	{
+		$product_details=array();
+		foreach($subCategory as $subC)
+		{
+			$single_product_detail=array();
+
+			$sub_cat_id=$subC['sub_cat_id'];
+			$subCatNameArray = getsubCategoryById($sub_cat_id);
+			$subCatName = $subCatNameArray['sub_cat_name'];
+
+			$quantity_id = $subC['quantity_id'];
+			$quantityDetails = getQuantityById($quantity_id);
+			$quantity = $quantityDetails['quantity'];
+
+
+			$unit_id = $subC['product_unit_id'];
+			$unitDetails = getUnitById($unit_id);
+			$unit_name = $unitDetails['unit_name'];
+
+			$price = $subC['customer_price'];
+
+
+			$attribute_type_names_array=getAttributeTypesForASubCatOfAnEnquiry($sub_cat_id,$enquiry_form_id);
+
+
+			$single_product_detail['product_name']=$subCatName;	//product name			
+
+			$types=array();
+
+			foreach($attribute_type_names_array as $attribute_type_names)
+			{
+				//type
+				$single_type=array();
+				$single_type[]=$attribute_type_names['attribute_type']. " : " .$attribute_type_names['attribute_names_string'];    
+				array_push($types,$single_type);              			
+
+			}
+
+			$single_product_detail["type"]=$types;
+
+
+			$single_product_detail["quantity"]=$quantity; 
+
+			$single_product_detail["price"]= $price. " ". $unit_name;
+
+
+
+		}
+
+		array_push($product_details, $single_product_detail);
+		
+
+	}
+
+
+
+	$enquiry_details=array();
+
+
+	if($enquiryDetails['customer_type_id']==NULL)
+	{
+		$EnquiryType =  "Not Available";
+	}
+	else
+	{
+		$customerTypeId = $enquiryDetails['customer_type_id'];
+		$customerTypeDetails = getCustomerTypeById($customerTypeId);
+		$EnquiryType= $customerTypeDetails['customer_type'];
+	}
+
+	$enquiry_details['enquiry_type']=$EnquiryType;
+
+
+	if($enquiryDetails['customer_type_id']==3)
+	{
+
+
+		$refrence_details = getRefrenceForEnquiryId($enquiry_form_id);
+		if($refrence_details['refrence_name'] != NULL)
+		{
+			$refrence_details= $refrence_details['refrence_name'];	 
+		}
+
+		$enquiry_details['refrence_name']=$refrence_details;
+
+	}
+
+	if($enquiryDetails['budget']==0)
+	{
+		$CustomerBudget= "Not Available"; 
+	}
+	else
+	{
+		$CustomerBudget= $enquiryDetails['budget']; 
+	}
+
+	$enquiry_details['customer_budget']=$CustomerBudget;
+
+
+	$discussion = $enquiryDetails['enquiry_discussion'];
+
+	if(!validateForNull($discussion))
+	{
+		$discussion= "No Discussion Available!"; 
+	}
+
+	$enquiry_details['discussion']=$discussion;
+
+
+
+
+	if(date('d/m/Y H:i:s', strtotime($enquiryDetails['follow_up_date']))=="01/01/1970")
+	{
+		$fstFollowUpDate= "Reminder not set."; 
+	}
+	else
+	{
+		$fstFollowUpDate= date('d/m/Y H:i:s', strtotime($enquiryDetails['follow_up_date']));
+	}
+
+	$enquiry_details['fst_follow_up_date']=$fstFollowUpDate;
+
+	$DateofEnquiry=date('d/m/Y H:i:s',strtotime($enquiryDetails['enquiry_date']));
+
+	$enquiry_details['date_of_enquiry']=$DateofEnquiry;
+
+	$adminUserID = $enquiryDetails['created_by']; 
+	$adminUserDetails = getAdminUserByID($adminUserID);
+
+	$EnquiryAddedBy= $adminUserDetails['admin_name'];
+
+	$enquiry_details['enquiry_added_by']=$EnquiryAddedBy;
+
+	$holderAdminID = $enquiryDetails['current_lead_holder']; 
+	$adminUserDetails = getAdminUserByID($holderAdminID);
+
+	$EnquiryCurrentlyHandledBy= $adminUserDetails['admin_name'];
+
+	$enquiry_details['enquiry_currently_handled_by']=$EnquiryCurrentlyHandledBy;
+
+
+
+
+
+
+	$root=array(
+		"enquiry"=>$enquiry,
+		"customer"=>$customer_detail_array,
+		"contact"=>getCustomerContactNo($customer_id),
+		"follow_up_details"=>getFollowUpDetailsByEnquiryId($enquiry_form_id),
+		"product_details"=>$product_details,
+		"enquiry_details"=>$enquiry_details);
+
 	echo json_encode($root);
-	
-	//echo json_encode(getExtraCustomerDetailsById($customer_id));
 
-	//echo json_encode(getCustomerById($customer_id));
+		//echo json_encode(getExtraCustomerDetailsById($customer_id));
 
-	
-	//echo json_encode(getFollowUpDetailsByEnquiryId($enquiry_form_id));
+		//echo json_encode(getCustomerById($customer_id));
 
-	
-	//echo json_encode(getVisitDetailsByEnquiryId($enquiry_form_id));
 
-	
-	//echo json_encode(getCloseLeadByEnquiryId($enquiry_form_id));
+		//echo json_encode(getFollowUpDetailsByEnquiryId($enquiry_form_id));
 
-	
-	//echo json_encode(getRelSubCatEnquiryFromEnquiryId($enquiry_form_id));
 
-	
-	//echo json_encode(getNotesByEnquiryId($enquiry_form_id));
+		//echo json_encode(getVisitDetailsByEnquiryId($enquiry_form_id));
 
-	
-	//echo json_encode(getBookingFormByEnquiryId($enquiry_form_id));
+
+		//echo json_encode(getCloseLeadByEnquiryId($enquiry_form_id));
+
+
+		//echo json_encode(getRelSubCatEnquiryFromEnquiryId($enquiry_form_id));
+
+
+		//echo json_encode(getNotesByEnquiryId($enquiry_form_id));
+
+
+		//echo json_encode(getBookingFormByEnquiryId($enquiry_form_id));
 
 	break;
 
@@ -78,13 +261,13 @@ switch($_POST["method"]){
 		$mobile_no=json_decode($_POST['mobile_no']);
 		$quantity_id=json_decode($_POST['quantity_id']);
 		$product_id=json_decode($_POST['product_id']);
-	    $mrp_array=json_decode($_POST['mrp']);
+		$mrp_array=json_decode($_POST['mrp']);
 		$unit_id=json_decode($_POST['unit_id']);
-		
+
 		$enquiry_group_id=explode(",", $_POST['enquiry_group_id']); 
 
 		$result=insertLead($_POST["prefix_id"], $_POST["customer_name"], $product_id, $mrp_array, $unit_id, $quantity_id,$attribute_name_array, $mobile_no, $_POST["email_id"], $_POST["discussion"], $_POST["customer_type_id"], $_POST["refrence"], $_POST["reminder_date"]. " ".$_POST["reminder_time"], $_POST['enquiry_date'], $_POST["budget"],$_POST["customer_id"], $_POST["city"], $_POST["customer_area"], $_POST["km"], $_POST["sms_status"], $enquiry_group_id);
-		
+
 		if(is_numeric($result) && is_numeric($_POST["customer_id"]))
 		{
 			$response=array("status"=>"1","message"=>"New Enquiry successfully added!");
@@ -180,7 +363,14 @@ switch($_POST["method"]){
 	$admin_id=$_SESSION['EMSadminSession']['admin_id'];
 	$data=viewFollowUps(getTodaysDate(),null,null,null,null,null,null,null,null,null,null,null,null,$admin_id);
 
-				//print_r($data);
+
+
+
+	//echo $data["next_follow_up_date"];
+
+	//$data["discussion"]=$exp[1];
+
+	//print_r($data);
 
 	uasort($data, "EMIPaymentDatesComparatorForEmiReports");
 
@@ -188,8 +378,21 @@ switch($_POST["method"]){
 
 	foreach($data as $d)
 	{
+		$exp=explode("^", $d["next_follow_up_date"]);
+
+		$exp1 = explode("#", $exp[1]);
+
+		$discussion = $exp1[0];
+		$handled_by = $exp1[1];
+		$d['discussion'] = $discussion;
+		$d['handled_by'] = $handled_by;
 		$new_data[] = $d;
 	}	
+
+
+	//print_r($new_data);
+
+
 	echo json_encode($new_data);
 
 	break;
@@ -225,8 +428,6 @@ switch($_POST["method"]){
 	if(dbNumRows($result)>0)
 	{
 
-
-
 		$admin=$adminArray[0];
 		$admin_id=$admin['admin_id'];
 		$admin_name=$admin['admin_name'];
@@ -240,13 +441,10 @@ switch($_POST["method"]){
 		if($resultt==0)
 		{
 
-
-
 			$_SESSION['EMSadminSession']['admin_name']=$adminArray[0]['admin_name'];
 			$_SESSION['EMSadminSession']['admin_id']=$adminArray[0]['admin_id'];
 			$_SESSION['EMSadminSession']['admin_rights']=getAdminRightsForAdminId($admin_id);
 			$_SESSION['EMSadminSession']['admin_logged_in']=true;
-
 
 
 			$sql="UPDATE 
